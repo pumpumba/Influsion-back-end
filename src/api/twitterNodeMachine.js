f726ef7114ce71b19d165899100c5006b37b9d8c
Ludwig Thaung <ludwig.thaung@gmail.com>
2018-09-28 11:48:28 +0200

    Removed some more unnecessary code in youtube and twitter API

--- src/api/twitterNodeMachine.js
+++ src/api/twitterNodeMachine.js
@@ -2,27 +2,16 @@
 require("dotenv").load();
 var Twitter = require("twitter");
 
-const consumerKeyPumba = process.env.TWITTER_CONSUMER_KEY;
-const consumerSecretPumba = process.env.TWITTER_CONSUMER_SECRET;
-const accessTokenPumba = process.env.TWITTER_ACCESS_TOKEN;
-const accessSecretPumba = process.env.TWITTER_ACCESS_SECRET;
-const bearerTokenPumba = process.env.TWITTER_BEARER_TOKEN;
-
 var client = new Twitter({
-  consumer_key: consumerKeyPumba,
-  consumer_secret: consumerSecretPumba,
-  access_token: accessTokenPumba,
-  access_secret: accessSecretPumba,
-  bearer_token: bearerTokenPumba
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_secret: process.env.TWITTER_ACCESS_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN
 });
 
 module.exports = {
   getUserTweets: function(username, tweetsAmount, callback) {
-    var client = new Twitter({
-      consumer_key: consumerKeyPumba,
-      consumer_secret: consumerSecretPumba,
-      bearer_token: bearerTokenPumba
-    });
     client.get(
       "/statuses/user_timeline",
       { screen_name: username, count: tweetsAmount },
@@ -32,12 +21,6 @@ module.exports = {
     );
   },
   getTweet: function(tweet_id, callback) {
-    var client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      bearer_token: process.env.TWITTER_BEARER_TOKEN
-    });
-
     var result = client.get("/statuses/show", { id: tweet_id }, function(
       error,
       tweets,
