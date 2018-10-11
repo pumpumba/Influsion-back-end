ee2a878a21ca04775d4bbd7b9d9a3d06811ae2a8
Erik Ståhl <e.stahl95@gmail.com>
2018-10-11 17:40:30 +0200

    fixed error with local node machines

--- src/api/twitterCloudComponent.js
+++ src/api/twitterCloudComponent.js
@@ -1,7 +1,7 @@
-module.exports = (function(){
-  var express = require('express');
-  //var Twitter = require('../../machinepack-twitternodemachines')
-  var Twitter = require('machinepack-twitternodemachines');
+module.exports = (function() {
+  var express = require("express");
+  var Twitter = require("../../machinepack-twitternodemachines");
+  //var Twitter = require('machinepack-twitternodemachines');
   const bodyParser = require("body-parser");
 
   app = express.Router();
@@ -16,76 +16,80 @@ module.exports = (function(){
     res.status(200);
     res.send("HTTP response 200 code OK.");
   });
-  
+
   app.get("/filters", (req, res) => {
     var filterType = req["query"]["filterType"];
     var assetType = req["query"]["assetType"];
-    switch(assetType) {
-      case 'tweet':
-        switch(filterType) {
-          case 'user':
-              res.json(['Popular', '<enter your influencers username>']);
-              break;
+    switch (assetType) {
+      case "tweet":
+        switch (filterType) {
+          case "user":
+            res.json(["Popular", "<enter your influencers username>"]);
+            break;
           default:
-              res.json(['Nothing available']);
+            res.json(["Nothing available"]);
         }
         break;
       default:
-        res.json(['Nothing available']);
+        res.json(["Nothing available"]);
     }
   });
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
 
   app.use(bodyParser.json());
 
   app.post("/content", (req, res) => {
     var inputObj = req.body;
     var context = inputObj.context;
-    if(inputObj.filterType == undefined) {
+    if (inputObj.filterType == undefined) {
       res.json({ errorMessage: "You need to provide a filterType" });
     }
     var filterTypes = inputObj.filterType;
-    if(inputObj.assetType == undefined ) {
+    if (inputObj.assetType == undefined) {
       res.json({ errorMessage: "You need to provide an assetType" });
     }
     var assetTypes = inputObj.assetType;
-    if(inputObj.filterValue == undefined ) {
+    if (inputObj.filterValue == undefined) {
       var filterValue = "";
-    }
-    else {
+    } else {
       var filterValue = inputObj.filterValue;
     }
-    if(inputObj.context == undefined ) {
+    if (inputObj.context == undefined) {
       var context = "";
-    }
-    else {
+    } else {
       var context = inputObj.context;
     }
-    if(isNaN(inputObj.offset)) {
+    if (isNaN(inputObj.offset)) {
       var offset = 0;
-    }
-    else {
+    } else {
       var offset = parseInt(inputObj.offset, 10);
     }
-    if(isNaN(inputObj.limit)) {
+    if (isNaN(inputObj.limit)) {
       var limit = 5;
-    }
-    else {
+    } else {
       var limit = parseInt(inputObj.limit, 10);
     }
     var resultObj = [];
     var successFlag = false;
-    for(var i = 0;i<assetTypes.length;i++) {
-      switch(assetTypes[i]) {
+    for (var i = 0; i < assetTypes.length; i++) {
+      switch (assetTypes[i]) {
         case "tweet":
-          for(var j = 0;j<filterTypes.length;j++) {
-            switch(filterTypes[j]) {
+          for (var j = 0; j < filterTypes.length; j++) {
+            switch (filterTypes[j]) {
               case "user":
-                switch(filterValue) {
-                  case 'Popular':
-                    var screenNames = ["elonmusk", "justinbieber", "barackobama", "potus", "billgates"];
+                switch (filterValue) {
+                  case "Popular":
+                    var screenNames = [
+                      "elonmusk",
+                      "justinbieber",
+                      "barackobama",
+                      "potus",
+                      "billgates"
+                    ];
                     Twitter.getPopularTweets({
                       consumerKey: process.env.TWITTER_CONSUMER_KEY,
                       consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
@@ -94,7 +98,7 @@ module.exports = (function(){
                       bearerToken: process.env.TWITTER_BEARER_TOKEN,
                       screenNames: screenNames,
                       count: limit
-                    }).exec({
+                    }).switch({
                       error: function(err) {
                         console.log("Error at getPopularTweets");
                         console.log(err);
@@ -103,11 +107,11 @@ module.exports = (function(){
                         res.json(result); //Shouldn't return json here yet, should do at the end but doesn't work at the moment cause it hangs in the loop for some reason.
                         successFlag = true;
                         //console.log("Length: " + result.length); // The loops also have to be changed in order for resultObj to contain right objects, because of async functions it won't output the right array of content at the moment.
-                        for(var k = 0; k<result.length;k++) {
+                        for (var k = 0; k < result.length; k++) {
                           resultObj.push(result[k]);
                         }
                       }
-                    })
+                    });
                     break;
                   default:
                     Twitter.getUserTweets({
@@ -118,7 +122,7 @@ module.exports = (function(){
                       bearerToken: process.env.TWITTER_BEARER_TOKEN,
                       screenName: filterValue,
                       count: limit
-                    }).exec({
+                    }).switch({
                       error: function(err) {
                         console.log("Error at getPopularTweets");
                         console.log(err);
@@ -130,24 +134,27 @@ module.exports = (function(){
                         successFlag = true;
                         res.json(result);
                       }
-                    })
+                    });
                 }
                 break;
               default:
-                res.json({ errorMessage: "The cloud component failed to provide any content" });
+                res.json({
+                  errorMessage:
+                    "The cloud component failed to provide any content"
+                });
             }
           }
           break;
         default:
-          res.json({ errorMessage: "The cloud component failed to provide any content" });
+          res.json({
+            errorMessage: "The cloud component failed to provide any content"
+          });
       }
     }
   });
-  
+
   app.get("/search", (req, res) => {
-    res.send(
-      "This request is currently not available"
-    );
+    res.send("This request is currently not available");
   });
 
   return app;
