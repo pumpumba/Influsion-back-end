5e5fac7cd4ac453822b065c276ab0e3c316ca9f7
JonatanBjurenfalk <jonatanbjurenfalk@gmail.com>
2018-11-02 14:57:53 +0100

    Now we get formated objects of instagram posts.

--- src/api/instagram.js
+++ src/api/instagram.js
@@ -1,12 +1,23 @@
 const https = require("https");
 require("dotenv").config({ path: __dirname + "/./../../.env" });
-const token = process.env.INSTAGRAM_TOKEN;
-var url =
-  "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + token;
-var instagramResponse;
+
+var formatedInstaPosts = "";
+var instagramResponse = "";
+
+
+const search = 'katyperry';
+
 
 module.exports = {
-  getInsta: function(callback) {
+  getInstaPosts: function(username, postCount, callback) {
+    getInstaPosts(username, postCount, callback);
+  }
+};
+
+function getInstaPosts(username, postCount, callback) {
+
+    var url = "https://graph.facebook.com/" + instagram_id + "?fields=business_discovery.username(" + search + "){id, name, followers_count, profile_picture_url, media.limit(5){id,permalink,caption,timestamp,like_count,media_url}}&access_token=" + access_token;
+
     https
       .get(url, function(res) {
         var body = "";
@@ -16,12 +27,38 @@ module.exports = {
         });
 
         res.on("end", function() {
-          instagramResponse = JSON.parse(body);
+          instagramResponse = formatJson(JSON.parse(body));
         });
       })
       .on("error", function(e) {
         console.log("Got an error: ", e);
       });
     callback(instagramResponse);
+
   }
-};
+
+function formatJson(instagramResponse) {
+  var formatedInstaPosts = []
+
+  for (var i = 0; i < instagramResponse.business_discovery.media.data.length; i++) {
+    var instaPost = {
+      "platform": "Instagram",
+      "user_id": instagramResponse.business_discovery.id,
+      "user_url": "",
+      "user_name": instagramResponse.business_discovery.name,
+      "user_screen_name": instagramResponse.business_discovery.username,
+      "user_followers_count": instagramResponse.business_discovery.followers_count,
+      // "user_verified": instagramResponse[i].user.verified,
+      "user_profile_image_url": instagramResponse.business_discovery.profile_picture_url,
+      "post_id": instagramResponse.business_discovery.media.data[i].id,
+      "post_url": instagramResponse.business_discovery.media.data[i].permalink,
+      "post_text": instagramResponse.business_discovery.media.data[i].caption,
+      "post_created_at": instagramResponse.business_discovery.media.data[i].timestamp
+    };
+
+    // instaPost.user_url = "https://instagram.com/" + instaPost.user_screen_name;
+
+    formatedInstaPosts.push(instaPost);
+  }
+  return formatedInstaPosts;
+}
